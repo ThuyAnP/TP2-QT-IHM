1006a3239fecfdab7d7460ff62ad90328f6a8a10
ThuyAnP <thuy_an.pham@lyceeppr.fr>
2023-12-11 10:57:36 +0100

    11ème Commit
    TP2 QT IHM 2. Concevoir un formulaire personnalisé pour notre application - Questions 1, 2, 3, 4, 5, 6 et 7 - FINI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->FormMessage = new MaForme() ;
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_BoutonArret_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Voulez vous arrêter?");
+    msgBox.setInformativeText("Appuyez sur OK pour quitter l'application");
+    msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::No);
+    int ret = msgBox.exec();
+    // On vérifie si l'utilisateur a cliqué sur Ok
+
+    if ( ret == QMessageBox::Ok )
+    {
+        //l'application se femre et donc tout s'éteint
+        close();
+    }
+    else
+    {
+        //seulement le message d'arrêt qui se ferme
+    }
+}
+
+void MainWindow::testForm()
+{
+    QPushButton *clickedButton = qobject_cast<QPushButton *>(sender());
+    clickedButton->setText("affichage form widget");
+    bool vis = this->FormMessage->isVisible();
+    this->FormMessage->setVisible(true);
+    vis = this->FormMessage->isVisible();
+    this->FormMessage->show();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "maforme.h"
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -14,8 +15,13 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void testForm();
+
+private slots:
+    void on_BoutonArret_clicked();
 
 private:
     Ui::MainWindow *ui;
+    MaForme *FormMessage;
 };
 #endif // MAINWINDOW_H
